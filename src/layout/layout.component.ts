43f37800557331fa05b4d1faa0889aa95f8db2af
faizan99904 <faizan99904@gmail.com>
2025-07-02 10:14:44 +0500

    add component

--- src/layout/layout.component.ts
+++ src/layout/layout.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { SidebarComponent } from '../shared/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { BottomNavComponent } from '../shared/bottom-nav/bottom-nav.component';
+import { BetslipComponent } from "../shared/betslip/betslip.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, SidebarComponent, RouterOutlet],
+  imports: [HeaderComponent, SidebarComponent, RouterOutlet, BottomNavComponent, BetslipComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
---
+++ src/shared/betslip/betslip.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-betslip',
+  imports: [],
+  templateUrl: './betslip.component.html',
+  styleUrl: './betslip.component.css'
+})
+export class BetslipComponent {
+
+}
