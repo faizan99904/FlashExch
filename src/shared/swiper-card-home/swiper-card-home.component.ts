2d7b5db5ad9055e0adaa7b31cca9e08bdd3500b0
Bilal Ahmad <bilal.codemaster@gmail.com>
2025-07-02 12:44:48 +0500

    added

---
+++ src/shared/swiper-card-home/swiper-card-home.component.ts
@@ -0,0 +1,13 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var Swiper: any;
+
+@Component({
+  selector: 'app-swiper-card-home',
+  standalone: true,
+  templateUrl: './swiper-card-home.component.html',
+  styleUrl: './swiper-card-home.component.css'
+})
+export class SwiperCardHomeComponent {
+
+}
