f08d90a8739829dc90867a3154380ae1f6721f4a
Maaz Bukhari <maazbukhari1144@gmail.com>
2025-07-02 16:47:10 +0500

    sport nav added!

--- src/layout/layout.component.ts
+++ src/layout/layout.component.ts
@@ -5,10 +5,11 @@ import { RouterOutlet } from '@angular/router';
 import { BottomNavComponent } from '../shared/bottom-nav/bottom-nav.component';
 import { BetslipComponent } from "../shared/betslip/betslip.component";
 import { SportsNavComponent } from "../shared/sports-nav/sports-nav.component";
+import { SportEventsComponent } from "../shared/sport-events/sport-events.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, SidebarComponent, RouterOutlet, BottomNavComponent, BetslipComponent, SportsNavComponent],
+  imports: [HeaderComponent, SidebarComponent, RouterOutlet, BottomNavComponent, BetslipComponent, SportsNavComponent, SportEventsComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
