a713108df3b6b0597b28a923ae6f1e37a8ce824a
faizan99904 <faizan99904@gmail.com>
2025-07-03 17:38:29 +0500

    work on desktop market

--- src/pages/sports-book/sports-book.component.ts
+++ src/pages/sports-book/sports-book.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SwiperCardHomeComponent } from '../../shared/swiper-card-home/swiper-card-home.component';
 import { SportEventsComponent } from '../../shared/sport-events/sport-events.component';
 import { SportslistComponent } from "../../shared/sportslist/sportslist.component";
+import { MarketComponent } from '../../component/market/market.component';
 
 
 @Component({
   selector: 'app-sports-book',
-  imports: [SwiperCardHomeComponent, SportEventsComponent, SportslistComponent],
+  imports: [SwiperCardHomeComponent, SportEventsComponent, SportslistComponent, MarketComponent],
   templateUrl: './sports-book.component.html',
   styleUrl: './sports-book.component.css',
 })
