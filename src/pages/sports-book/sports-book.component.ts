ce5d912d5d5cff435134a6176e41fe82b01e52fa
Maaz Bukhari <maazbukhari1144@gmail.com>
2025-07-03 13:25:45 +0500

    refactored sportnav!

--- src/pages/sports-book/sports-book.component.ts
+++ src/pages/sports-book/sports-book.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { SwiperCardHomeComponent } from "../../shared/swiper-card-home/swiper-card-home.component";
+import { SwiperCardHomeComponent } from '../../shared/swiper-card-home/swiper-card-home.component';
 import { SportEventsComponent } from '../../shared/sport-events/sport-events.component';
-
 
 @Component({
   selector: 'app-sports-book',
   imports: [SwiperCardHomeComponent, SportEventsComponent],
   templateUrl: './sports-book.component.html',
-  styleUrl: './sports-book.component.css'
+  styleUrl: './sports-book.component.css',
 })
-export class SportsBookComponent {
-
-}
+export class SportsBookComponent {}
