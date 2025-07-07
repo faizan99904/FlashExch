34620a6b7b8908465765a69ad52fc8e5984adda0
AdeebX-hash <m.adeeb.7497@gmail.com>
2025-07-07 13:08:26 +0500

    fix market

--- src/pages/market-detail/market-detail.component.ts
+++ src/pages/market-detail/market-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-market-detail',
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './market-detail.component.css'
 })
 export class MarketDetailComponent {
-
+  constructor(private location: Location){}
+  navigateBack() {
+    this.location.back();
+  }
 }
