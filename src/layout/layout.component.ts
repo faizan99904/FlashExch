436d967d4d59f6767b2363e5c222e155af416707
Maaz Bukhari <maazbukhari1144@gmail.com>
2025-07-07 17:40:11 +0500

    signup page added!

--- src/layout/layout.component.ts
+++ src/layout/layout.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { SidebarComponent } from '../shared/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
 import { BottomNavComponent } from '../shared/bottom-nav/bottom-nav.component';
-import { BetslipComponent } from "../shared/betslip/betslip.component";
-import { SportsNavComponent } from "../shared/sports-nav/sports-nav.component";
-import { SportEventsComponent } from "../shared/sport-events/sport-events.component";
-import { MobileSidebarComponent } from "../shared/mobile-sidebar/mobile-sidebar.component";
+import { BetslipComponent } from '../shared/betslip/betslip.component';
+import { SportsNavComponent } from '../shared/sports-nav/sports-nav.component';
+import { SportEventsComponent } from '../shared/sport-events/sport-events.component';
+import { MobileSidebarComponent } from '../shared/mobile-sidebar/mobile-sidebar.component';
+import { SignupComponent } from '../auth/signup/signup.component';
+import { SharedService } from '../app/service/shared.service';
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, SidebarComponent, RouterOutlet, BottomNavComponent, BetslipComponent, SportsNavComponent, MobileSidebarComponent],
+  imports: [
+    HeaderComponent,
+    SidebarComponent,
+    RouterOutlet,
+    BottomNavComponent,
+    BetslipComponent,
+    SportsNavComponent,
+    MobileSidebarComponent,
+    SignupComponent,
+  ],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
 export class LayoutComponent {
+  constructor(private toggle: SharedService) {}
 
+  signUp = computed(() => this.toggle.isSignupVisible());
 }
