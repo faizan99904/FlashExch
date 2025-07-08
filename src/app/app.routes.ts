f3940770bbaf82384322e8158fc1be686f8bf950
Bilal Ahmad <bilal.codemaster@gmail.com>
2025-07-08 17:46:46 +0500

    feat(racing): add racing component with styles, template, and functionality
    - Implemented RacingComponent with HTML structure for displaying racing events.
    - Added CSS styles for layout, banners, filters, and match listings.
    - Included functionality to toggle odds display.
    - Created unit tests for RacingComponent to ensure proper instantiation.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { SportsBookComponent } from '../pages/sports-book/sports-book.component'
 import { LoginComponent } from '../auth/login/login.component';
 import { MarketDetailComponent } from '../pages/market-detail/market-detail.component';
 import { CasinoComponent } from '../pages/casino/casino.component';
+import { RacingComponent } from '../pages/racing/racing.component';
 
 export const routes: Routes = [
   {
@@ -13,7 +14,7 @@ export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    children: [{ path: '', component: SportsBookComponent }, { path: 'market-detail', component: MarketDetailComponent }, ],
+    children: [{ path: '', component: SportsBookComponent }, { path: 'market-detail', component: MarketDetailComponent }, { path: 'racing', component: RacingComponent },],
   },
   { path: 'casino', component: CasinoComponent }
 ];
