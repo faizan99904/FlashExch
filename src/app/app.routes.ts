93ce4d3817cb6ce039ec8fa86bd2ff0746e3642f
Maaz Bukhari <maazbukhari1144@gmail.com>
2025-07-08 22:23:57 +0500

    forget password page added!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,21 @@ import { SportsBookComponent } from '../pages/sports-book/sports-book.component'
 import { LoginComponent } from '../auth/login/login.component';
 import { MarketDetailComponent } from '../pages/market-detail/market-detail.component';
 import { CasinoComponent } from '../pages/casino/casino.component';
+import { ForgetPasswordComponent } from '../auth/forget-password/forget-password.component';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
   },
+  { path: 'passwordrecovery', component: ForgetPasswordComponent },
   {
     path: '',
     component: LayoutComponent,
-    children: [{ path: '', component: SportsBookComponent }, { path: 'market-detail', component: MarketDetailComponent }, ],
+    children: [
+      { path: '', component: SportsBookComponent },
+      { path: 'market-detail', component: MarketDetailComponent },
+    ],
   },
-  { path: 'casino', component: CasinoComponent }
+  { path: 'casino', component: CasinoComponent },
 ];
---
+++ src/auth/forget-password/forget-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/header/header.component";
+
+@Component({
+  selector: 'app-forget-password',
+  imports: [HeaderComponent],
+  templateUrl: './forget-password.component.html',
+  styleUrl: './forget-password.component.css'
+})
+export class ForgetPasswordComponent {
+
+}
