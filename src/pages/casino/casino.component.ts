546dbe58b35a0a83f6f40cf17f00a5b8efb8e85c
faizan99904 <faizan99904@gmail.com>
2025-07-09 15:13:12 +0500

    complete casino desktop view

--- src/pages/casino/casino.component.ts
+++ src/pages/casino/casino.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { CommonModule } from '@angular/common';
+import { SportsNavComponent } from "../../shared/sports-nav/sports-nav.component";
 declare var Swiper: any;
 @Component({
   selector: 'app-casino',
-  imports: [HeaderComponent, CommonModule],
+  imports: [HeaderComponent, CommonModule, SportsNavComponent],
   templateUrl: './casino.component.html',
   styleUrl: './casino.component.css',
   // encapsulation: ViewEncapsulation.None
