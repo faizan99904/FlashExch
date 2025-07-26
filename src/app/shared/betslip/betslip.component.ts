ff29abadc10fce8b56fb3d1607866f6c95762af5
Maaz Bukhari <maazbukhari1144@gmail.com>
2025-07-27 01:19:39 +0500

    editstakes functionality added!

--- src/app/shared/betslip/betslip.component.ts
+++ src/app/shared/betslip/betslip.component.ts
@@ -10,10 +10,17 @@ import { FormsModule } from '@angular/forms';
 })
 export class BetslipComponent {
   stake: string = '';
+  profit!: number;
+  editStakeMode = false;
+  stakeButtonsBeforeLogin = [100, 200, 500, 1000, 2000, 5000];
+  stakeButtonsAfterLogin = [1000, 2000, 5000];
+  editableStakes: number[] = [];
+  odds: number = 1.58;
   @Input() color: string = '';
 
   ngOnInit(): void {
     this.loginState();
+    this.editableStakes = [...this.getCurrentStakeButtons()];
   }
 
   login: any;
@@ -26,5 +33,73 @@ export class BetslipComponent {
       this.login = false;
     }
     return this.login;
+  }
+
+  addStake(amount: number): void {
+    const current = parseInt(this.stake || '0', 10);
+    const validCurrent = isNaN(current) ? 0 : current;
+    const newStake = validCurrent + amount;
+    this.stake = String(newStake);
+    this.updateProfit();
+  }
+
+  updateProfit(): void {
+    setTimeout(() => {
+      const parsedStake = parseInt(this.stake || '0', 10);
+      const validStake = isNaN(parsedStake) ? 0 : parsedStake;
+      this.profit = validStake * 2;
+    });
+  }
+
+  trackByIndex(index: number, item: any): number {
+    return index;
+  }
+
+  setMinStake(): void {
+    this.stake = '100';
+    this.profit = 100 * 2;
+  }
+
+  setMaxStake(): void {
+    this.stake = '250000';
+    this.profit = 250000 * 2;
+  }
+
+  clearStake(): void {
+    this.stake = '';
+    this.profit = 0;
+  }
+
+  getCurrentStakeButtons(): number[] {
+    return this.login
+      ? this.stakeButtonsAfterLogin
+      : this.stakeButtonsBeforeLogin;
+  }
+
+  toggleEditStake(): void {
+    this.editStakeMode = !this.editStakeMode;
+    this.editableStakes = [...this.getCurrentStakeButtons()];
+  }
+
+  updateStake(index: number, value: number) {
+    this.editableStakes[index] = value;
+  }
+
+  saveEditedStakes(): void {
+    if (this.login) {
+      this.stakeButtonsAfterLogin = [...this.editableStakes];
+    } else {
+      this.stakeButtonsBeforeLogin = [...this.editableStakes];
+    }
+    this.editStakeMode = false;
+  }
+
+  increaseOdds() {
+    this.odds = parseFloat((this.odds + 0.01).toFixed(2));
+  }
+
+  decreaseOdds() {
+    const newOdds = this.odds - 0.01;
+    this.odds = newOdds > 0 ? parseFloat(newOdds.toFixed(2)) : this.odds;
   }
 }
