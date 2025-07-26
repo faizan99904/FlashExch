8aa0dc5d1fe54cabe42f9148f8d925c092befb59
faizan99904 <faizan99904@gmail.com>
2025-07-26 10:50:32 +0500

    change password integration done

--- src/app/pages/accont/change-password/change-password.component.ts
+++ src/app/pages/accont/change-password/change-password.component.ts
@@ -3,6 +3,8 @@ import { CommonModule, Location } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { CONFIG } from '../../../../../config';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-change-password',
   imports: [ReactiveFormsModule, CommonModule],
@@ -10,9 +12,10 @@ import { CONFIG } from '../../../../../config';
   styleUrl: './change-password.component.css'
 })
 export class ChangePasswordComponent {
-  changePassword!: FormGroup
+  changePassword!: FormGroup;
+  confirmPasswordMatch: boolean = false
 
-  constructor(private location: Location, private fb: FormBuilder, private http: HttpClient) {
+  constructor(private location: Location, private fb: FormBuilder, private http: HttpClient, private toaster: ToastrService, private router: Router) {
     this.changePassword = this.fb.group({
       newPassword: ['', Validators.required],
       confirmPassword: ['', Validators.required],
@@ -24,27 +27,54 @@ export class ChangePasswordComponent {
     this.location.back();
   }
 
-  passwordMatchValidator(group: FormGroup) {
-    const newPass = group.get('newPassword')?.value;
-    const confirmPass = group.get('confirmPassword')?.value;
-    return newPass === confirmPass ? null : { passwordMismatch: true };
+  checkMatchPassword() {
+    const confirm = this.changePassword.get('confirmPassword')?.value
+    const newPass = this.changePassword.get('newPassword')?.value
+    if (confirm != newPass) {
+      this.confirmPasswordMatch = true
+    } else {
+      this.confirmPasswordMatch = false
+    }
   }
 
+
+
   onSubmit() {
-    // CONFIG.userChangePassword
+
     const req = {
       oldPassword: this.changePassword.get('oldPassword')?.value,
       newPassword: this.changePassword.get('newPassword')?.value
     }
-    this.http.post(CONFIG.userChangePassword, req).subscribe({
-      next: (res: any) => {
 
-      },
+    if(this.changePassword.valid){
+      this.http.post(CONFIG.userChangePassword, req).subscribe({
+        next: (res: any) => {
+          this.toaster.success(res.meta.message);
+          localStorage.clear();
+          this.router.navigate(['/']);
+          this.changePassword.reset();
+        },
 
-      error: (error: any) => {
+        error: (error: any) => {
+          let errorObject = error.meta ? error.meta.message : error.error?.meta?.message;
+          if (typeof errorObject === 'object') {
+            for (var key of Object.keys(errorObject)) {
+              this.toaster.error(errorObject[key].message, '', {
+                positionClass: 'toast-top-right',
+              });
+              return;
+            }
+          } else {
+            this.toaster.error(errorObject, '', {
+              positionClass: 'toast-top-right',
+              timeOut: 800,
+            });
+            return;
+          }
+        }
+      })
+    }
 
-      }
-    })
   }
 
 
