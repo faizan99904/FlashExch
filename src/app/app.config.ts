33d36185eb665b64605cea901726b9485f87cfa0
Aoun-Universal <78606381+Aoun-Javaid@users.noreply.github.com>
2025-07-28 12:05:26 +0400

    changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, NgZone, PLATFORM_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
@@ -8,6 +8,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 
 import { routes } from './app.routes';
+import { AngularFirestoreBinary, AngularFirestoreCricket, AngularFirestoreOther, AngularFirestoreScore, AngularFirestoreSoccer, AngularFirestoreTennis } from './service/firebase.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,7 +17,37 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
-    DatePipe
+    DatePipe,
+    {
+      provide: 'firebaseProjectCricket',
+      deps: [PLATFORM_ID, NgZone],
+      useFactory: AngularFirestoreCricket
+  },
+  {
+      provide: 'firebaseProjectTennis',
+      deps: [PLATFORM_ID, NgZone],
+      useFactory: AngularFirestoreTennis
+  },
+  {
+      provide: 'firebaseProjectSoccer',
+      deps: [PLATFORM_ID, NgZone],
+      useFactory: AngularFirestoreSoccer
+  },
+  {
+      provide: 'firebaseProjectBinary',
+      deps: [PLATFORM_ID, NgZone],
+      useFactory: AngularFirestoreBinary
+  },
+  {
+      provide: 'firebaseProjectOther',
+      deps: [PLATFORM_ID, NgZone],
+      useFactory: AngularFirestoreOther
+  },
+  {
+      provide: 'firebaseProjectScore',
+      deps: [PLATFORM_ID, NgZone],
+      useFactory: AngularFirestoreScore
+  },
   ]
 };
 
