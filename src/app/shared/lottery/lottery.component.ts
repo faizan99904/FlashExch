54eb2a92539b585bdb6482a52cb1c2f7c8ddf50e
Maaz Bukhari <maazbukhari1144@gmail.com>
2025-07-29 13:06:43 +0500

    lottery component added!

---
+++ src/app/shared/lottery/lottery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lottery',
+  imports: [],
+  templateUrl: './lottery.component.html',
+  styleUrl: './lottery.component.css'
+})
+export class LotteryComponent {
+
+}
