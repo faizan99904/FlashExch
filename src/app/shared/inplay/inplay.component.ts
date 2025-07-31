4b771bd223fe041f0c065f1b8f31e27e1459e77f
Maaz Bukhari <maazbukhari1144@gmail.com>
2025-07-31 16:38:25 +0500

    inplay design is in progress!

---
+++ src/app/shared/inplay/inplay.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inplay',
+  imports: [],
+  templateUrl: './inplay.component.html',
+  styleUrl: './inplay.component.css'
+})
+export class InplayComponent {
+
+}
