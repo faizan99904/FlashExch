4859084124427d3eaba70e672213a38a9eb0b8f8
faizan99904 <faizan99904@gmail.com>
2025-08-13 17:53:16 +0500

    dynamic sport page done

--- src/app/pages/sport/sport.component.ts
+++ src/app/pages/sport/sport.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
+import { MainService } from '../../service/main.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sport',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sport.component.html',
   styleUrl: './sport.component.css'
 })
 export class SportComponent {
+  sidebarEvent: any;
+  subSportList: any = [];
+  sportIndex: any
+  isSportList: boolean = true
+
+  constructor(private mainService: MainService, private router:Router) {
+    effect(() => {
+      this.sidebarEvent = mainService.getSideBarEvent();
+    })
+  }
+
+  filterSubSport(sportId: any) {
+    this.isSportList = false;
+    this.subSportList = [...this.sidebarEvent?.data].filter((event: any) => {
+      return sportId === event.sportId
+    })
+  }
+
+  openListChild(index: any) {
+    if (this.sportIndex == index) {
+      this.sportIndex = null
+    } else {
+      this.sportIndex = index
+    }
+
+  }
+
+  gotoMarket(market: any) {
+    localStorage.setItem('competitionName', market.tournamentName);
+    this.router.navigateByUrl(
+      '/market-detail/' + market.sportId + '/' + market.exEventId
+    );
+  }
 
 }
