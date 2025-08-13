96bc928eb77c5d4e4055c1d70bc3517a429b7fd9
faizan99904 <faizan99904@gmail.com>
2025-08-13 12:49:54 +0500

    fix my bets issues

--- src/app/shared/bottom-nav/bottom-nav.component.ts
+++ src/app/shared/bottom-nav/bottom-nav.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { CONFIG } from '../../../../config';
 import { NetworkService } from '../../service/network.service';
 import { LoaderComponent } from '../loader/loader.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bottom-nav',
@@ -15,23 +16,42 @@ import { LoaderComponent } from '../loader/loader.component';
 })
 export class BottomNavComponent {
   exposureData: any[] = [];
+  loader: boolean = false
   bets: boolean = false;
   betInfo: boolean = false;
-
+  checkBoxIndex: any
   constructor(
     private sharedService: SharedService,
-    private backendService: NetworkService
-  ) {
-    effect(() => {
-      this.exposureData = (this.sharedService.getExposureData()() ?? []).map(
-        (item: any) => ({
+    private backendService: NetworkService,
+    private http: HttpClient
+  ) { }
+
+
+  getUserEventExposure() {
+    this.loader = true;
+    this.http.post(CONFIG.userEventExposure, {}).subscribe({
+      next: (res: any) => {
+        // Apply the same transformation logic that was in the effect
+        this.exposureData = (res.data ?? []).map((item: any) => ({
           ...item,
           openBets: false,
           loader: false,
-          matchedBetList: [],
-        })
-      );
+          matchedBetList: []
+        }));
+        this.loader = false;
+      },
+      error: (error) => {
+        console.log(error);
+        this.loader = false;
+      },
     });
+  }
+
+  toggleBets() {
+    this.bets = !this.bets;
+    if (this.bets) {
+      this.getUserEventExposure()
+    }
   }
 
   formatDate(dateString: string): string {
@@ -40,10 +60,17 @@ export class BottomNavComponent {
     return date.toLocaleString();
   }
 
-  toggleBet(item: any) {
+  toggleBet(item: any, index: number) {
+
+    if (this.checkBoxIndex == index) {
+      this.checkBoxIndex = null
+    } else {
+      this.checkBoxIndex = index
+    }
+
     item.openBets = !item.openBets;
     if (item.openBets && item.matchedBetList.length === 0) {
-      item.loader = true;
+      this.loader = true;
       let req = {
         eventId: item.eventId,
         sportId: item.sportId,
@@ -53,10 +80,10 @@ export class BottomNavComponent {
         .then(
           (record: any) => {
             item.matchedBetList = record.data;
-            item.loader = false;
+            this.loader = false;
           },
           () => {
-            item.loader = false;
+            this.loader = false;
           }
         );
     }
@@ -65,4 +92,6 @@ export class BottomNavComponent {
   openSidebar() {
     this.sharedService.mobileSidebarOpen();
   }
+
+
 }
