8ae3a19d10691c8cee5428d25b7e9b9bb3438f66
faizan99904 <faizan99904@gmail.com>
2025-08-16 11:45:32 +0500

    add logout api

--- src/app/pages/account-nav/account-nav.component.ts
+++ src/app/pages/account-nav/account-nav.component.ts
@@ -1,7 +1,10 @@
 import { Component, effect } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { NetworkService } from '../../service/network.service';
 import { SharedService } from '../../service/shared.service';
+import { CONFIG } from '../../../../config';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-account-nav',
@@ -13,10 +16,29 @@ export class AccountNavComponent {
   userBalance: any;
   constructor(
     public networkService: NetworkService,
-    public toggle: SharedService
+    public toggle: SharedService,
+    private toaster: ToastrService,
+    private http: HttpClient,
+    private router:Router
   ) {
     effect(() => {
       this.userBalance = this.networkService.getUserBalanceSignal()();
     });
+  }
+
+  // userLogout
+  logout() {
+    this.http.post(CONFIG.userLogout, {}).subscribe({
+      next: (res: any) => {
+        this.toaster.success(res.meta.message);
+        localStorage.clear();
+        this.toggle.setToken(null);
+        // this.token = null
+        this.router.navigateByUrl('/')
+      },
+      error: (err) => {
+        this.toaster.success(err.error.message)
+      }
+    })
   }
 }
